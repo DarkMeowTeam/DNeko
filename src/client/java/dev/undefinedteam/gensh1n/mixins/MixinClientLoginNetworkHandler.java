ab8141ff07c50e006db95cab415b3dd8acdd32fd
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2024-11-26 18:41:53 +0800

    refactor(Inject\Mixin): Heypixel Protocol build remove warn

--- src/client/java/dev/undefinedteam/gensh1n/mixins/MixinClientLoginNetworkHandler.java
+++ src/client/java/dev/undefinedteam/gensh1n/mixins/MixinClientLoginNetworkHandler.java
@@ -1,16 +1,17 @@
 package dev.undefinedteam.gensh1n.mixins;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.undefinedteam.gensh1n.system.modules.Modules;
 import dev.undefinedteam.gensh1n.system.modules.misc.Protocol;
+import net.minecraft.client.ClientBrandRetriever;
 import net.minecraft.client.network.ClientLoginNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientLoginNetworkHandler.class)
 public class MixinClientLoginNetworkHandler {
-    @ModifyExpressionValue(method = "onSuccess", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/ClientBrandRetriever;getClientModName()Ljava/lang/String;"))
-    private String getClientModName(String original) {
-        return Modules.get().get(Protocol.class).isHeypixel() ? "forge" : original;
+    @Redirect(method = "onSuccess", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/ClientBrandRetriever;getClientModName()Ljava/lang/String;"))
+    private String getClientModName() {
+        return Modules.get().get(Protocol.class).isHeypixel() ? "forge" : ClientBrandRetriever.getClientModName();
     }
 }
